4eb0bc3b39c4403106d2852ff4afd4a622c160ed
leonardodiasc <leonardodiass@gmail.com>
2023-02-10 13:50:55 -0300

    adicionado validações para nome, cpf, e rg

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve possuir 11 dígitos")
-        return cpf
-    
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"O CPF deve ter 11 dígitos"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Não inclua números neste campo"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':"O RG deve ter 9 dígitos"})
+        return data
