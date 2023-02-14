c8d04a58afc39219c09f7d744579d6c069cafcda
leonardodiasc <leonardodiass@gmail.com>
2023-02-14 11:53:06 -0300

    adicionado validação de número de celular de acordo com padrão estabelecido

--- clientes/validators.py
+++ clientes/validators.py
@@ -1,3 +1,5 @@
+import re
+
 def cpf_valido(numero_do_cpf):
     return len(numero_do_cpf) == 11
 
@@ -6,3 +8,9 @@ def nome_valido(nome):
 
 def rg_valido(numero_do_rg):
     return len(numero_do_rg) == 9
+
+def celular_valido(numero_celular):
+    """Verifica se o celular é valido"""
+    modelo = '[0-9]{2} [0-0]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, numero_celular)
+    return resposta
