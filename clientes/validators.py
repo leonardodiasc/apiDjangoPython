db6147441ec0aa867575d2931d8dfd907f23f541
leonardodiasc <leonardodiass@gmail.com>
2023-02-15 10:26:26 -0300

    adicionado script

--- clientes/validators.py
+++ clientes/validators.py
@@ -1,7 +1,9 @@
 import re
+from validate_docbr import CPF
 
 def cpf_valido(numero_do_cpf):
-    return len(numero_do_cpf) == 11
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
 
 def nome_valido(nome):
     if(nome.isalpha() or nome.isspace):
