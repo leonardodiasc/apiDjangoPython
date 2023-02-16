775dfd77bf8cfd52aeeeeddd6bb3e179eec83109
leonardodiasc <leonardodiass@gmail.com>
2023-02-16 11:31:24 -0300

    adicionado filtros

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes'
+    'clientes',
+    'django_filters',
+
 ]
 
 MIDDLEWARE = [
